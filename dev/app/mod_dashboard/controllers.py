57278b4e48026708b936a7b58e7477f4210fe661
Stephane Truong <stephane.truong@gmail.com>
2016-07-01 21:22:52 +0800

    recommit

--- dev/app/mod_dashboard/controllers.py
+++ dev/app/mod_dashboard/controllers.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, render_template, \
                   jsonify
 from flask.ext.login import (current_user, login_required, login_user, logout_user, confirm_login, fresh_login_required)
 from app import app
-import json
+import json, pandas
 import models
 
 # Define the blueprint: 'dashboard', set its url prefix: app.url/dashboard
@@ -21,10 +21,19 @@ def make_session_permanent():
 @login_required
 def dashboard():
 
-	
+	customerJson=json.dumps(models.customer_stats.objects.to_json())
+	df= pandas.read_json(eval(customerJson))
+	migrationsStacked = df.groupby(['category','category2016']).agg({'ads' : 'sum'}) 
+	migrations = migrationsStacked.unstack(level=-1)
+	migrations = migrations.fillna(0)
+	migrations.columns= migrations.columns.droplevel()
+	migrationJson = migrations.apply(lambda x: 100*x/float(x.sum()),axis=1).round(2).to_json()
+	print migrationJson
+
 	template_data= {
-		'customerData':json.dumps(models.customer_stats.objects.to_json())
-			}
+		'customerData': customerJson,
+		'migrationData': migrationJson
+		}
 
 	return render_template("dashboard/index.html", **template_data)
 
